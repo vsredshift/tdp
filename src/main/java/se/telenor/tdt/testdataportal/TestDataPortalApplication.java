4c3a5a33e5b99725c7e491c9eb928cb5c570522f
Martin O Shaughnessy <vsredhsift@gmail.com>
2021-08-27 22:35:00 +0200

    Initial commit

---
+++ src/main/java/se/telenor/tdt/testdataportal/TestDataPortalApplication.java
@@ -0,0 +1,13 @@
+package se.telenor.tdt.testdataportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestDataPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestDataPortalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/telenor/tdt/testdataportal/TestDataPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package se.telenor.tdt.testdataportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestDataPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
